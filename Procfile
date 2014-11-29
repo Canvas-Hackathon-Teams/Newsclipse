25a2567c709d8782d433c8920a4b59671c49b903
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-29 21:28:45 +0300

    Very basic calais extractor.

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: gunicorn --preload --log-file - newsclipse.manage:app
 dev: python newsclipse/manage.py runserver
+worker: celery -A newsclipse.queue worker
--- newsclipse/web.py
+++ newsclipse/web.py
@@ -3,9 +3,11 @@ from restpager import Pager
 
 from newsclipse.core import app
 from newsclipse.util import obj_or_404
+from newsclipse.queue import extract, lookup
 
 
 @app.route('/')
 def home():
+    extract.delay('huhu')
     #pager = Pager(search_block(q))
-    return render_template("index.html") #, pager=pager)
+    return render_template("index.html")
