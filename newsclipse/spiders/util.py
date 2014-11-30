cf251bf45ea61aff8a1f1019947d64ea972e5ffb
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-30 14:25:52 +0300

    main class.

--- newsclipse/spiders/util.py
+++ newsclipse/spiders/util.py
@@ -3,13 +3,16 @@
 class Spider(object):
 
     def search_generic(self, story, card):
-        return card
+        return self.search_all(story, card)
 
     def search_person(self, story, card):
-        return card
+        return self.search_all(story, card)
 
     def search_company(self, story, card):
-        return card
+        return self.search_all(story, card)
 
     def search_organization(self, story, card):
+        return self.search_all(story, card)
+
+    def search_all(self, story, card):
         return card
