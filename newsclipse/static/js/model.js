f8d79c059398263cf72923f62d146c822b51d118
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-30 13:56:22 +0300

    set attribute ID names.

--- newsclipse/static/js/model.js
+++ newsclipse/static/js/model.js
@@ -3,9 +3,9 @@
 // ===================================================================
 
 App.Card = Backbone.Model.extend({ 
-
+  attributeId: '_id'
 });
 
 App.Story = Backbone.Model.extend({ 
-
+  attributeId: '_id'
 });
